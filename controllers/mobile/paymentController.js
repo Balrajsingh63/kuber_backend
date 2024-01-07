00c5bfa831cfd50f90df80f6accdc2c32afc9266
Amit Mandad <bs718205@gmail.com>
2024-01-07 19:15:48 +0530

    merge code

--- controllers/mobile/paymentController.js
+++ controllers/mobile/paymentController.js
@@ -26,7 +26,8 @@ class PaymentController {
                 date: new Date(),
                 transactionId,
                 paymentStatus,
-                gameRequestId
+                amount: amount,
+                gameRequestId: gameRequestId != "" ? gameRequestId : null
             })
             let transactions = await Transaction.create({
                 userId: req.user._id,
@@ -36,7 +37,8 @@ class PaymentController {
                 status: "active"
             });
             const user = await userModel.findOne({ "_id": req.user._id });
-            await user.updateOne({ wallet: user.wallet + Number(amount) });
+            await user.updateOne({ wallet: Number(user.wallet) + Number(amount) });
+            console.log(user);
             return res.json({
                 status: true,
                 message: "Payment successfully done."
