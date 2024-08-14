29587a5a27fb3ddaecc0887f16bb23826f772093
jagtarsinghwork <jagtarsinghwork@gmail.com>
2024-08-14 11:30:51 +0530

    admin registor api create api

--- controllers/adminAuthController.js
+++ controllers/adminAuthController.js
@@ -1,37 +1,65 @@
+/** @format */
+
 const userModel = require("../models/userModel");
 const msg = require("../uility/constant");
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const saltRounds = 10;
 
 class AdminAuthController {
-    async login(req, res) {
-        let { email, password } = req.body;
-        let user = await userModel.findOne({ email: email, role: "Admin" }).lean();
-        if (!user) {
-            return res.status(200).json({
-                status: false,
-                message: msg.Invalid_Record,
-                data: []
-            });
-        }
-        const match = await bcrypt.compare(password, user.password);
-        if (!match) {
-            return res.status(200).json({
-                status: false,
-                message: msg.Password_Not_Match,
-                data: []
-            });
-        }
-        let token = jwt.sign(user, process.env.privateKey);
-        user.token = token;
-        return res.status(200).json({
-            status: true,
-            message: msg.login_success,
-            data: user
-        });
+	async AdminRegister(req, res) {
+		try {
+			const { email, password } = req.body;
+			let userCheck = await userModel.findOne({ email: email });
+			if (userCheck) {
+				return res.json({
+					status: false,
+					message: "Admin already exist on this email",
+				});
+			}
+			await userModel.create({
+				email: email,
+				password: await bcrypt.hashSync(password, saltRounds),
+				role: "Admin",
+			});
+			res.status(201).json({
+				status: true,
+				message: "User register successfully.",
+			});
+		} catch (error) {
+			console.log(error.message);
+			return res.status(500).json({
+				message: "Somthing want worng ! try again later",
+			});
+		}
+	}
 
-    }
+	async login(req, res) {
+		let { email, password } = req.body;
+		let user = await userModel.findOne({ email: email, role: "Admin" }).lean();
+		if (!user) {
+			return res.status(200).json({
+				status: false,
+				message: msg.Invalid_Record,
+				data: [],
+			});
+		}
+		const match = await bcrypt.compare(password, user.password);
+		if (!match) {
+			return res.status(200).json({
+				status: false,
+				message: msg.Password_Not_Match,
+				data: [],
+			});
+		}
+		let token = jwt.sign(user, process.env.privateKey);
+		user.token = token;
+		return res.status(200).json({
+			status: true,
+			message: msg.login_success,
+			data: user,
+		});
+	}
 }
 
 module.exports = new AdminAuthController();
--- routes/adminAuthRoute.js
+++ routes/adminAuthRoute.js
@@ -1,8 +1,10 @@
-var express = require('express');
+/** @format */
+
+var express = require("express");
 var router = express.Router();
 const AuthRouter = require("../controllers/adminAuthController");
 
 router.post("/login", AuthRouter.login);
-// router.post("/user-register", AuthRouter.userRegister);
+router.post("/admin-register", AuthRouter.AdminRegister);
 
 module.exports = router;
